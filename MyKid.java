a8f17f5395fbcd730de079a493fedcdf8eb6b614
VijayalakshmiCebi <88899106+VijayalakshmiCebi@users.noreply.github.com>
2021-08-27 21:21:39 +0530

    Add files via upload

---
+++ MyKid.java
@@ -0,0 +1,13 @@
+package Week1.Day2;
+
+public class MyKid {
+
+	public static void main(String[] args) {
+		MyMobile newObj = new MyMobile();
+		newObj.makeCalls();
+		newObj.sendMsgs();
+		//newObj.payBills();
+
+	}
+
+}
